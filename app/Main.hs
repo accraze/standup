0cfb27700a6bbf874fa95bd619bf8a1d41db601b
accraze <accraze@gmail.com>
2018-01-10 08:55:27 -0800

    adding sub parser for opts

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,42 @@
-module Main where
+module Main (main) where
 
 import Options.Applicative
 
-main :: IO ()
-main = putStrLn "hello world!"
+type ItemIndex = Int
+type ItemDescription = Maybe String
 
+data Options = Options FilePath ItemIndex ItemDescription deriving Show
+
+defaultDataPath :: FilePath
+defaultDataPath = "~/.standup.yaml"
+
+optionsParser :: Parser Options
+optionsParser = Options
+    <$> dataPathParser
+    <*> itemIndexParser
+    <*> updateItemDescriptionParser
+
+dataPathParser :: Parser FilePath
+dataPathParser = strOption $
+    value defaultDataPath
+    <> long "data-path"
+    <> short 'p'
+    <> metavar "DATAPATH"
+    <> help ("path to data file (default " ++ defaultDataPath ++ ")")
+
+itemIndexParser :: Parser ItemIndex
+itemIndexParser = argument auto (metavar "ITEMINDEX" <> help "index of item")
+
+itemDescriptionValueParser :: Parser String
+itemDescriptionValueParser =
+    strOption (long "desc" <> short 'd' <> metavar "DESCRIPTION" <> help "description")
+
+updateItemDescriptionParser :: Parser ItemDescription
+updateItemDescriptionParser = 
+    Just <$> itemDescriptionValueParser
+    <|> flag' Nothing (long "clear-desc") -- "--clear-desc"
+
+main :: IO ()
+main = do
+    options <- execParser (info (optionsParser) (progDesc "Standup manager"))
+    putStrLn $ "options=" ++ show options
